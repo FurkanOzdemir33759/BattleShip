465a0c55c41da3c1d1ccf2201a921fe82e7ed3f3
FurkanOzdemir33759 <furkan.ozdemir.33759@ozu.edu.tr>
2024-04-26 15:06:18 +0300

    Game finish info implemented.

--- GameManager/GameFinishInfo.java
+++ GameManager/GameFinishInfo.java
@@ -16,8 +16,53 @@ public class GameFinishInfo {
      */
     private final int score;
 
-    public GameFinishInfo(Player winner, int score) {
-        this.winner = winner.ordinal();
+    /**
+     * Constructor for the GameFinishInfo. Takes a winner and a score.
+     * @param winner The winner. 0 for player one, 1 for player two or AI.
+     * @param score The score of the winner.
+     */
+    public GameFinishInfo(int winner, int score) {
+        this.winner = winner;
         this.score = score;
+    }
+
+    /**
+     * Constructor for the GameFinishInfo. Takes a winner enum and a score.
+     * @param winner The winner. P1 for player one, P2 for player two or AI.
+     * @param score The score of the winner.
+     */
+    public GameFinishInfo(Player winner, int score) {
+        this(winner.ordinal(),  score);
+    }
+
+    /**
+     * Gets the winner as int. 0 => P1, 1 => P2/AI
+     * @return The winner as int.
+     */
+    public int getWinner() {
+        return winner;
+    }
+
+    /**
+     * Gets the winner as enum. P1 => 0, P2 => 1
+     * @return The winner as enum.
+     */
+    public Player getWinnerAsEnum() throws IllegalArgumentException {
+        switch (winner) {
+            case 0:
+                return Player.P1;
+            case 1:
+                return Player.P2;
+            default:
+                throw new IllegalArgumentException("Invalid winner number: " + winner);
+        }
+    }
+
+    /**
+     * Gets the score of the winner.
+     * @return The score of the winner.
+     */
+    public int getScore() {
+        return score;
     }
 }
