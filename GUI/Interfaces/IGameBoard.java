f049a0d7b2a4f738290afb923b3bf4ff2039e09f
iremkatman <irem.katman@ozu.edu.tr>
2024-05-10 19:11:04 +0300

    Added javadoc comments to interfaces in the gui package.

--- GUI/Interfaces/IGameBoard.java
+++ GUI/Interfaces/IGameBoard.java
@@ -1,4 +1,23 @@
 package GUI.Interfaces;
 
+/**
+ * Represents the game board where the game is played.
+ */
 public interface IGameBoard {
+    /**
+     * Places a ship on the game board.
+     *
+     * @param x the x-coordinate of the ship
+     * @param y the y-coordinate of the ship
+     * @param orientation the orientation of the ship (e.g., "horizontal" or "vertical")
+     */
+    void placeShip(int x, int y, String orientation);
+
+    /**
+     * Attacks a specific location on the game board.
+     *
+     * @param x the x-coordinate of the attack
+     * @param y the y-coordinate of the attack
+     */
+    void attack(int x, int y);
 }
--- GUI/Interfaces/INotification.java
+++ GUI/Interfaces/INotification.java
@@ -1,4 +1,16 @@
 package GUI.Interfaces;
 
+/**
+ * Manages notifications related to game events.
+ */
 public interface INotification {
+    /**
+     * Displays a notification when a hit occurs.
+     */
+    public void displayHitMiss();
+
+    /**
+     * Displays the results of the game.
+     */
+    public void displayGameResults();
 }
