73b2c58511fa1e231342f78c6fe07690e00c7ffa
Umut Ardıl Murat <108032181+RenaudDeCraon@users.noreply.github.com>
2024-05-13 22:31:06 +0300

    Minor changes made in MainManu GUI

--- GUI/MainMenu.java
+++ GUI/MainMenu.java
@@ -2,12 +2,20 @@ package GUI;
 
 import GUI.Interfaces.IMainMenu;
 
+
 import javax.swing.*;
+import java.awt.*;
 
 public class MainMenu extends JPanel implements IMainMenu {
+
+
+
+
+
     @Override
     public void startGame() {
         // Implementation code here
+
 
     }
 
@@ -24,6 +32,35 @@ public class MainMenu extends JPanel implements IMainMenu {
     }
 
     MainMenu() {
-        add(new JLabel("MAIN MENU!"));
+        setLayout(null);
+        JLabel gameName = new JLabel("BATTLESHIP ");
+        gameName.setFont(new Font("Georgia",Font.BOLD,40));
+        gameName.setBounds(500,50,300,150);
+
+        JButton playButton = new JButton("PLAY");
+        playButton.setFont(new Font("Georgia", Font.PLAIN,20));
+        playButton.setBounds(540,200,200,50);
+
+        JButton settingsButton = new JButton("SETTINGS");
+        settingsButton.setFont(new Font("Georgia",Font.PLAIN,20));
+        settingsButton.setBounds(540,300,200,50);
+
+        JButton exitButton = new JButton("EXIT");
+        exitButton.setFont(new Font("Georgia", Font.PLAIN,20));
+        exitButton.setBounds(540,400,200,50);
+
+
+
+
+        add(gameName);
+        add(playButton);
+        add(settingsButton);
+        add(exitButton);
+
+
+
+
+
     }
+
 }
