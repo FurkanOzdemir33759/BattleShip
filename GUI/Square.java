c538e932483137842f1445beabacc0fe11c32767
iremkatman <irem.katman@ozu.edu.tr>
2024-05-14 13:53:16 +0300

    Updated the Square class to handle ship placement and attack status

--- GUI/Square.java
+++ GUI/Square.java
@@ -1,11 +1,48 @@
 package GUI;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Represents a square on the game board.
  */
-public class Square {
+public class Square extends JPanel {
     public int x;
     public int y;
     public boolean isOccupied;
     public boolean isHit;
+    public Square(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.isOccupied = false;
+        this.isHit = false;
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        setBackground(Color.BLUE);
+    }
+
+    public void setShip(boolean isOccupied) {
+        this.isOccupied = isOccupied;
+        setBackground(isOccupied ? Color.GRAY : Color.BLUE);
+    }
+
+    public boolean hasShip() {
+        return isOccupied;
+    }
+
+    public void setHit(boolean isHit) {
+        this.isHit = isHit;
+        setBackground(isHit ? Color.RED : (isOccupied ? Color.GRAY : Color.BLUE));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (isHit) {
+            g.setColor(Color.RED);
+            g.fillRect(0, 0, getWidth(), getHeight());
+        } else if (isOccupied) {
+            g.setColor(Color.GRAY);
+            g.fillRect(0, 0, getWidth(), getHeight());
+        }
+    }
 }
