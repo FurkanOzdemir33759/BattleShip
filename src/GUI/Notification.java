2212505f0a12d3aeac8d6915d4a2c0eefebbc3bc
guzerhan <guz.erhan@ozu.edu.tr>
2024-05-21 17:04:43 +0300

    PlayerInfo subclass added to Notification

--- src/GUI/Notification.java
+++ src/GUI/Notification.java
@@ -1,5 +1,7 @@
 package GUI;
 
+import GameManager.Enums.Player;
+
 import GUI.Interfaces.INotification;
 
 import javax.swing.*;
@@ -29,5 +31,48 @@ public class Notification extends JPanel implements INotification {
         notificationLabel.setText(result);
         notificationLabel.setForeground(Color.GREEN);
     }
+    class PlayerInfo extends JPanel {
+        private Player player;
+        private JLabel name = new JLabel();
+        private JLabel score = new JLabel("0");
+
+        public PlayerInfo(Player player) {
+            this.player = player;
+
+            setBackground(GUI_PALETTE.BACKGROUND);
+            GUI_Util.setAbsoluteSize(this, new Dimension(400, 50));
+            setLayout(null);
+
+            name.setFont(new Font("OCR A Extended", Font.BOLD, 24));
+            name.setText(player == Player.PLAYER ? "PLAYER" : "COMPUTER");
+            name.setHorizontalAlignment(SwingConstants.CENTER);
+            name.setVerticalAlignment(SwingConstants.CENTER);
+            name.setForeground(GUI_PALETTE.PLAYER_INFO_TEXT_COLOR);
+
+            score.setFont(new Font("OCR A Extended", Font.BOLD, 36));
+            score.setText(score.getText());
+            score.setVerticalAlignment(SwingConstants.CENTER);
+            score.setHorizontalAlignment(SwingConstants.CENTER);
+            score.setForeground(GUI_PALETTE.PLAYER_INFO_TEXT_COLOR);
+
+            if (player == Player.PLAYER) {
+                GUI_Util.placeAbsoluteComponent(name, 0, 0, 210, 50);
+                GUI_Util.placeAbsoluteComponent(score, 210, 0, 190, 50);
+            } else {
+                GUI_Util.placeAbsoluteComponent(name, 190, 0, 210, 50);
+                GUI_Util.placeAbsoluteComponent(score, 0, 0, 190, 50);
+            }
+
+            add(name);
+            add(score);
+        }
+        public  void updateScore( int score){
+            this.score.setText(""+score);
+            revalidate();
+            repaint();
+
+        }
+    }
+
 
 }
