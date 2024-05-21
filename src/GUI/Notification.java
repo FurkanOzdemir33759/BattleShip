5a8e8abe0b5f3969f2f351025f6417db2174c6b4
guzerhan <guz.erhan@ozu.edu.tr>
2024-05-21 17:21:40 +0300

    Updated Notification class

--- src/GUI/Notification.java
+++ src/GUI/Notification.java
@@ -5,6 +5,9 @@ import GameManager.Enums.Player;
 import GUI.Interfaces.INotification;
 
 import javax.swing.*;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 
 public class Notification extends JPanel implements INotification {
@@ -73,6 +76,107 @@ public class Notification extends JPanel implements INotification {
 
         }
     }
+    public class GameInfo extends JPanel {
+        private JLabel phaseText = new JLabel();
+        private JButton returnButton , skipPlacementPhaseButton;
+
+        public GameInfo() {
+            setBackground(GUI_PALETTE.BACKGROUND);
+            GUI_Util.setAbsoluteSize(this, new Dimension(420, 50));
+            setLayout(null);
+
+            returnButton = new JButton("QUIT");
+            GUI_Util.placeAbsoluteComponent(returnButton, 0, 0, 100, 50);
+            returnButton.setFont(new Font("SansSerif", Font.PLAIN, 24));
+            returnButton.setFocusPainted(false);
+            returnButton.setFocusable(false);
+            returnButton.setBackground(GUI_PALETTE.BACKGROUND);
+            returnButton.setForeground(GUI_PALETTE.PLAYER_INFO_TEXT_COLOR);
+            returnButton.setBorder(new CompoundBorder(new LineBorder(GUI_PALETTE.GRID_BORDER), new EmptyBorder(2, 5, 2, 5)));
+
+
+            skipPlacementPhaseButton = new JButton("START");
+            GUI_Util.placeAbsoluteComponent(skipPlacementPhaseButton, 320, 0, 100, 50);
+            skipPlacementPhaseButton.setFocusPainted(false);
+            skipPlacementPhaseButton.setFont(new Font("SansSerif", Font.PLAIN, 24));
+            skipPlacementPhaseButton.setBackground(GUI_PALETTE.BACKGROUND);
+            skipPlacementPhaseButton.setForeground(GUI_PALETTE.PLAYER_INFO_TEXT_COLOR);
+            skipPlacementPhaseButton.setBorder(new CompoundBorder(new LineBorder(GUI_PALETTE.GRID_BORDER), new EmptyBorder(2, 5, 2, 5)));
+
+
+            GUI_Util.placeAbsoluteComponent(phaseText, 100, 0, 220, 50);
+            phaseText.setForeground(GUI_PALETTE.PLAYER_INFO_TEXT_COLOR);
+            phaseText.setBackground(GUI_PALETTE.BACKGROUND);
+            phaseText.setText("SET YOUR BOARD!");
+            phaseText.setHorizontalAlignment(SwingConstants.CENTER);
+            phaseText.setVerticalAlignment(SwingConstants.CENTER);
+            phaseText.setFont(new Font("SansSerif", Font.PLAIN, 18));
+
+
+            add(returnButton);
+            add(skipPlacementPhaseButton);
+            add(phaseText);
+        }
+
+        public JButton getReturnButton() {
+            return returnButton;
+        }
+
+        public JButton getSkipPlacementPhaseButton() {
+            return skipPlacementPhaseButton;
+        }
+
+        public JLabel getPhaseText() {
+            return phaseText;
+        }
+    }
+    public class Result extends JPanel {
+
+        public  Result(Player winner, int playerScore , int aiScore ){
+            setBackground(GUI_PALETTE.BACKGROUND);
+            setLayout(null);
+
+            JLabel playerScoreLabel = new JLabel("Player Score : " + playerScore);
+            GUI_Util.placeAbsoluteComponent(playerScoreLabel, 100, 100, 540, 200);
+            playerScoreLabel.setHorizontalAlignment(SwingConstants.CENTER);
+            playerScoreLabel.setVerticalAlignment(SwingConstants.CENTER);
+            playerScoreLabel.setFont(new Font("Monospaced", Font.ITALIC, 50));
+            playerScoreLabel.setForeground(GUI_PALETTE.PLAYER_INFO_TEXT_COLOR);
+
+
+            JLabel aiScoreLabel = new JLabel("AI Score : " + aiScore);
+            GUI_Util.placeAbsoluteComponent(aiScoreLabel, 640, 100, 540, 200);
+            aiScoreLabel.setHorizontalAlignment(SwingConstants.CENTER);
+            aiScoreLabel.setVerticalAlignment(SwingConstants.CENTER);
+            aiScoreLabel.setFont(new Font("Monospaced", Font.ITALIC, 50));
+            aiScoreLabel.setForeground(GUI_PALETTE.PLAYER_INFO_TEXT_COLOR);
+
+            String winner_text = "";
+            switch (winner){
+
+                case AI ->winner_text = "AI WINS!";
+                case PLAYER -> winner_text = "PlAYER WINS!";
+            }
+
+            JLabel winnerLabel = new JLabel(winner_text);
+            GUI_Util.placeAbsoluteComponent(winnerLabel, 440, 250, 400, 200);
+            winnerLabel.setHorizontalAlignment(SwingConstants.CENTER);
+            winnerLabel.setVerticalAlignment(SwingConstants.CENTER);
+            winnerLabel.setFont(new Font("Monospaced", Font.ITALIC, 50));
+            winnerLabel.setForeground(GUI_PALETTE.PLAYER_INFO_TEXT_COLOR);
+
+            add(winnerLabel);
+            add(playerScoreLabel);
+            add(aiScoreLabel);
+
+
+        }
+
+
+    }
+
+
+
 
 
 }
